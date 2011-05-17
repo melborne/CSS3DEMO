87be7e47d1655ef716dd1c38004f4ff90ebe71ff
kyoendo <postagie@gmail.com>
2011-05-18 07:55:35 +0900

    use configure dev for coffee file

--- app.rb
+++ app.rb
@@ -6,13 +6,15 @@ configure do
   APP_TITLE = "CSS3DEMO"
 end
 
+configure :development do
+  get '/javascripts/app.js' do
+   coffee :app
+  end
+end
+
 get '/' do
   haml :index
 end
-
-#get '/javascripts/app.js' do
- #coffee :app
-#end
 
 get '/style.css' do
   scss :style
